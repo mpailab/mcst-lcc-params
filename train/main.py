efa8b80872f700ad203da7ad426108311a82dc1e
BokovGrigoriy <bokovgrigoriy@gmail.com>
2019-05-15 14:26:51 +0300

    Update reading of arguments

--- train/main.py
+++ train/main.py
@@ -2,17 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # External imports
-import os, sys
+import argparse, os, sys
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from scipy import interpolate
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 # Internal imports
-import options
+
+
+#########################################################################################
+# Globals
 
 # Default compiler options
+#  option => (option's number, default value)
 PARS = {
     'ifconv_calls_num'          : ( 0, 6),
     'ifconv_merge_heur'         : ( 1, 1.0),
@@ -56,13 +62,38 @@ SPECS = [
     ]
 Ns = len(SPECS)
 
+# Objective function
 def F (Tc,Te,V):
     return Tc + 5*Te + V
 
+# Default value of objective function
 FL = F(1.0,1.0,1.0)
 
+#########################################################################################
 # Read script options
-opts = options.read(sys.argv[1:], PARS.keys(), SPECS)
+
+parser = argparse.ArgumentParser( prog = 'main.py',
+                                  description='Try to train neural network for compiler parameters')
+
+parser.add_argument( '-d', dest='dataset', metavar='<data_set>', default='./DataSet', 
+                     help='set a dataset directory')
+
+parser.add_argument( '-p', dest='pars', metavar='<par>', nargs='+', default=list(PARS.keys()), choices=list(PARS.keys()), 
+                     help='specify compiler parameters (values: %(choices)s; default: all parameters)')
+
+parser.add_argument( '-s', dest='specs', metavar='<spec>', nargs='+', default=SPECS, choices=SPECS, 
+                     help='specify compiled specs (values: %(choices)s; default: all specs)')
+
+parser.add_argument( '-i', dest='interp', metavar='<interp>', default='linear', choices=['linear', 'quadratic', 'cubic'],
+                     help='specify an order of spline interpolation (values: %(choices)s; default: %(default)s)')
+
+parser.add_argument( '-v', '--verbose', action='store_true', default=False,
+                     help='print more details')
+
+args = parser.parse_args()
+
+#########################################################################################
+# Read database
 
 class Spec:
     def __init__(self, name):
@@ -77,27 +108,28 @@ def read_eval (str):
     return (float(x[0]), F(float(x[1]), float(x[2]), float(x[3])))
     
 Table = {}
-for p in opts.pars:
+for p in args.pars:
     Table[p] = [0,[]]
 
-print('Spec initialization:')
-for name in opts.specs:
+print('Read database:')
+for name in args.specs:
 
-    if not os.path.exists(opts.dataset + '/Params/' + name + '.txt'):
-        print('Warning: ' + opts.dataset + '/Params does not contain ' + name + '.txt. Spec ' + '\'' + name + '\' will be omitted.')
+    if not os.path.exists(args.dataset + '/Params/' + name + '.txt'):
+        print('Warning: ' + args.dataset + '/Params does not contain ' + name + '.txt. Spec ' + '\'' + name + '\' will be omitted.')
         continue
 
-    if not os.path.exists(opts.dataset + '/Values/' + name + '.txt'):
-        print('Warning: ' + opts.dataset + '/Values does not contain ' + name + '.txt. Spec ' + '\'' + name + '\' will be omitted.')
+    if not os.path.exists(args.dataset + '/Values/' + name + '.txt'):
+        print('Warning: ' + args.dataset + '/Values does not contain ' + name + '.txt. Spec ' + '\'' + name + '\' will be omitted.')
         continue
 
     spec = Spec(name)
-    sys.stdout.write(name + ' ... ')
+    sys.stdout.write('  ' + name + ' ... ')
+    sys.stdout.flush()
 
-    with open(opts.dataset + '/Params/' + name + '.txt', 'r') as f:
+    with open(args.dataset + '/Params/' + name + '.txt', 'r') as f:
         spec.procs = [read_proc(line.rstrip('\n')) for line in f]
 
-    with open(opts.dataset + '/Values/' + name + '.txt', 'r') as f:
+    with open(args.dataset + '/Values/' + name + '.txt', 'r') as f:
         rows = [line.rstrip('\n') for line in f]
         while len(rows) < Np: rows.append('')
         for p in PARS.keys():
@@ -112,7 +144,7 @@ for name in opts.specs:
                     vs.sort()
                     x = [ i for i, j in vs ]
                     y = [ j for i, j in vs ]
-                    f = interpolate.interp1d(x, y, kind=opts.interp, fill_value="extrapolate")
+                    f = interpolate.interp1d(x, y, kind=args.interp, fill_value="extrapolate")
                     if Table[p][0] < vs[-1][0]: Table[p][0] = vs[-1][0]
                     Table[p][1].append([spec,f,x,y])
 
@@ -128,30 +160,16 @@ for name in opts.specs:
 #             plt.plot(x, y, '-', spec[2], spec[3], 'o')
 #             plt.show()
 
-for p in Table.keys():
+#########################################################################################
+# Train neural network
 
-    print (p + ':')
+def train (data, delta):
 
-    if Table[p][1] == []: continue
-
-    data = []
-    step = Table[p][0]/100
-    mask = x = np.arange(0, 101*step, step)
-    for ls in Table[p][1]:
-        spec = ls[0]
-        f = ls[1]
-        val = FL - f(mask)
-        val = np.maximum(val / max (val), 0)
-        for proc in spec.procs:
-            # data.append((proc[1:], val / (10000*proc[0] + 1)))
-            if proc[0] != 0:
-                data.append((proc[1:], val / proc[0]))
+    # print('Data size: ' + str(len(data)))
 
     np.random.shuffle(data)
 
-    print('Data size: ' + str(len(data)))
-
-    N = int(0.7*len(data))
+    N = int(delta*len(data))
 
     train_X = np.array([ i for i, j in data[0:N] ])
     train_Y = np.array([ j for i, j in data[0:N] ])
@@ -174,10 +192,41 @@ for p in Table.keys():
                    metrics=['categorical_accuracy'])
     model.fit(train_X, train_Y, epochs=100, batch_size=100, verbose=0)
     test_loss, test_acc = model.evaluate(test_X, test_Y, verbose=0)
-    print('Test accuracy:', test_acc)
+
+    # print('Test accuracy:', test_acc)
 
     # predictions=model.predict(test_X)
     # print(predictions[0])
     # print(test_Y[0])
     # plt.plot(mask, test_Y[0], '-', mask, predictions[0], 'o')
     # plt.show()
+
+    return test_acc
+
+print('Train neural network:')
+for p in Table.keys():
+
+    sys.stdout.write('  ' + p + ' ... ')
+    sys.stdout.flush()
+
+    if Table[p][1] == []: 
+        print('Not enough data available')
+        continue
+
+    data = []
+    step = Table[p][0]/100
+    mask = x = np.arange(0, 101*step, step)
+    for ls in Table[p][1]:
+        spec = ls[0]
+        f = ls[1]
+        val = FL - f(mask)
+        if max(val) != 0:
+            val = np.maximum(val / max (val), 0)
+        for proc in spec.procs:
+            # data.append((proc[1:], val / (10000*proc[0] + 1)))
+            if proc[0] != 0:
+                data.append((proc[1:], val / proc[0]))
+
+    acc = train(data, 0.7)
+    sys.stdout.write('ok (accuracy ' + str(acc) + ')\n')
+    
