b92e7c023024efeca798c47c41b0ab197a3ca671
alkonoval <alexandr.konoval@gmail.com>
2019-06-03 23:44:54 +0300

    Добавил преобразование из пользовательских имен во внутренние имена для параметров ИС в файл convert.py

---
+++ choice/convert.py
@@ -0,0 +1,266 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+    содержит преобразования пользовательских имен параметров ИС в имена внутренних переменных
+"""
+
+import global_vars as gl
+
+# brief_name -> (fullname, discription, type, values, format, default_value)
+data = {
+    'specs' : ('SPECS',
+        'задает список задач, для которых ИС должна осуществлять подборку параметров оптимизирующих преобразований',
+        'format',
+        None,
+        '<specname>[: <procname> [<procname>]] [, <specname>[: <procname> [<procname>]]]',
+        gl.__dict__['SPECS'] ),
+
+    'sync' : ('SYNCHRONOUS_OPTIMIZATION_FOR_SPECS',
+        'определяет характер оптимизации (синхронная или независимая) по отношению к заданному параметром specs списку задач',
+        'bool',
+        ['0', '1'], None,
+        int(gl.__dict__['SYNCHRONOUS_OPTIMIZATION_FOR_SPECS']) ),
+
+    'seq' : ('SEQ_OPTIMIZATION_WITH_STRATEGY',
+        'определяет характер оптимизации (последовательная или обособленная) по отношению к заданной параметром strategy стратегии оптимизации',
+        'bool',
+        ['0', '1'], None,
+        int(gl.__dict__['SEQ_OPTIMIZATION_WITH_STRATEGY']) ),
+
+    'strategy' : ('OPTIMIZATION_STRATEGY',
+        'задает стратегию оптимизации, согласно с которой ИС осуществляет подборку параметров оптимизирующих преобразований',
+        'format',
+        None,
+        '<parname> [<parname>] [; <parname> [<parname>]]',
+        gl.__dict__['OPTIMIZATION_STRATEGY'] ),
+
+    'every' : ('GAIN_STAT_ON_EVERY_OPTIMIZATION_STEP',
+        'включает пересчет статистики, собираемой компилятором LCC, при изменении значений параметров оптимизирующих преобразований',
+        'bool',
+        ['0', '1'], None,
+        int(gl.__dict__['GAIN_STAT_ON_EVERY_OPTIMIZATION_STEP']) ),
+
+    'start_temp' : ('START_TEMPERATURE',
+        'задает начальное значение температуры',
+        'float',
+        ['(0, 1]'], None,
+        gl.__dict__['START_TEMPERATURE'] ),
+
+    'temp_law' : ('TEMPERATURE_LAW_TYPE',
+        'задает закон убывания температуры',
+        'disc',
+        ['0', '1', '2'], None,
+        gl.__dict__['TEMPERATURE_LAW_TYPE'] ),
+
+    'alpha' : ('ALPHA_IN_TEPMERATURE_LAW',
+        'задает значение параметра alpha в законе убывания температуры',
+        'float',
+        ['(0, 1)'], None,
+        gl.__dict__['ALPHA_IN_TEPMERATURE_LAW'] ),
+
+    'distr_law' : ('DISTRIBUTION_LAW_TYPE',
+        'задает тип вероятностного распределения, определяющего выбор очередного кандидата для параметров оптимизирующих преобразований на каждом шаге метода имитации отжига',
+        'disc',
+        ['0', '1', '2', '3'], None,
+        gl.__dict__['DISTRIBUTION_LAW_TYPE'] ),
+
+    'relations' : ('USE_RELATIONS_OF_PARAMETORS',
+        'включает использование зависимостей параметров оптимизирующих параметров друг от друга',
+        'bool',
+        ['0', '1'], None,
+        int(gl.__dict__['USE_RELATIONS_OF_PARAMETORS']) ),
+
+    'F_exec' : ('TIME_EXEC_IMPOTANCE',
+        'задает значение множителя при члене относительного увеличении времени исполнения в минимизируемом функционале',
+        'float', None, None,
+        gl.__dict__['TIME_EXEC_IMPOTANCE'] ),
+
+    'F_comp' : ('TIME_COMP_IMPOTANCE',
+        'задает значение множителя при члене относительного увеличении времени компиляции в минимизируемом функционале',
+        'float', None, None,
+        gl.__dict__['TIME_COMP_IMPOTANCE'] ),
+
+    'F_mem' : ('MEMORY_IMPOTANCE',
+        'задает значение множителя при члене относительного увеличении объема, потребляемой компилятором памяти, в минимизируемом функционале',
+        'float', None, None,
+        gl.__dict__['MEMORY_IMPOTANCE'] ),
+
+    'comp_limit' : ('COMP_TIME_INCREASE_ALLOWABLE_PERCENT',
+        'задает допустимое относительное замедление времени компиляции',
+        'float', None, None,
+        gl.__dict__['COMP_TIME_INCREASE_ALLOWABLE_PERCENT'] ),
+
+    'exec_limit' : ('EXEC_TIME_INCREASE_ALLOWABLE_PERCENT',
+        'задает допустимое относительное замедление времени исполнения',
+        'float', None, None,
+        gl.__dict__['EXEC_TIME_INCREASE_ALLOWABLE_PERCENT'] ),
+
+    'mem_limit' : ('MEMORY_INCREASE_ALLOWABLE_PERCENT',
+        'задает допустимое относительное увеличение объема памяти, потребляемой при компиляции',
+        'float', None, None,
+        gl.__dict__['MEMORY_INCREASE_ALLOWABLE_PERCENT'] ),
+
+    'iter_num' : ('MAX_NUMBER_ITERATIONS',
+        'задает число шагов применения метода имитации отжига',
+        'int', None, None,
+        gl.__dict__['MAX_NUMBER_ITERATIONS'] ),
+
+    'attempts_num' : ('MAX_NUMBER_OF_ATTEMPTS_FOR_ITERATION',
+        'задает максимальное число попыток выбора очередного кандидата для параметров оптимизирующих преобразований на каждом шаге применения метода имитации отжига',
+        'int', None, None,
+        gl.__dict__['MAX_NUMBER_OF_ATTEMPTS_FOR_ITERATION'] ),
+
+    'decrease_temp' : ('DECREASE_TEMPERATURE_BEFORE_UNFORTUNATE_ITERATIONS',
+        'включает уменьшение температуры после неудачных шагов применения метода имитации отжига',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['DECREASE_TEMPERATURE_BEFORE_UNFORTUNATE_ITERATIONS']) ),
+
+    'max_dcs_level' : ('MAX_DCS_LEVEL',
+        'задает номер последнего существующего уровня оптимизации фазы dcs',
+        'int', None, None,
+        gl.__dict__['MAX_DCS_LEVEL'] ),
+
+    'dcs_loop' : ('DCS_KOEF_LOOP_IMPOTANCE',
+        'задает коэффициент мертвых циклов в формуле полезности dcs-оптимизации',
+        'float', None, None,
+        gl.__dict__['DCS_KOEF_LOOP_IMPOTANCE'] ),
+
+    'dcs_edge' : ('DCS_KOEF_EDGE_IMPOTANCE',
+        'задает коэффициент мертвых ребер в формуле полезности dcs-оптимизации',
+        'float', None, None,
+        gl.__dict__['DCS_KOEF_EDGE_IMPOTANCE'] ),
+
+    'dcs_node' : ('DCS_KOEF_NODE_IMPOTANCE',
+        'задает коэффициент мертвых узлов в формуле полезности dcs-оптимизации',
+        'float', None, None,
+        gl.__dict__['DCS_KOEF_NODE_IMPOTANCE'] ),
+
+    'dcs_limit' : ('DSC_IMPOTANCE_LIMIT',
+        'задает порог для значений полезности dcs-оптимиизации',
+        'float', None, None,
+        gl.__dict__['DSC_IMPOTANCE_LIMIT'] ),
+
+    'smooth' : ('SMOOTH_STAT',
+        'включает режим сглаживания распределений, получаемых по статистике работы компилятора LCC',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['SMOOTH_STAT']) ),
+
+    'erf_disc' : ('ERF_KOEF_FOR_DISCRETE_PAR',
+        'задает коэффициент сглаживания распределений параметров оптимизирующих преобразований целочисленного типа',
+        'float', None, None,
+        gl.__dict__['ERF_KOEF_FOR_DISCRETE_PAR'] ),
+
+    'erf_cont' : ('ERF_KOEF_FOR_CONTINUOUS_PAR',
+        'задает коэффициент сглаживания распределений параметров оптимизирующих преобразований вещественного типа',
+        'float', None, None,
+        gl.__dict__['ERF_KOEF_FOR_CONTINUOUS_PAR'] ),
+
+    'erf_limit' : ('ZERO_LIMIT_FOR_ERF',
+        'задает долю для сглаживаемых весов, которая не будет участвовать в сглаживании',
+        'float', 
+        ['(0, 1)'], None,
+        gl.__dict__['ZERO_LIMIT_FOR_ERF'] ),
+
+    'weight_limit' : ('ZERO_LIMIT_FOR_WEIGHT',
+        'задает порог для абсолютных значений весов, веса ниже которого не сглаживаются',
+        'float',
+        ['(0, 1)'], None,
+        gl.__dict__['ZERO_LIMIT_FOR_WEIGHT'] ),
+
+    'w_task' : ('TASK_WEIGHT_SETUP',
+        'задает режим учета весов задач при обработке статистики работы компилятора LCC',
+        'disc',
+        ['0', '1', '2', '3'], None,
+        gl.__dict__['TASK_WEIGHT_SETUP'] ),
+
+    'w_proc' : ('PROC_WEIGHT_SETUP',
+        'задает режим учета весов процедур при обработке статистики работы компилятора LCC',
+        'disc',
+        ['0', '1'], None,
+        gl.__dict__['PROC_WEIGHT_SETUP'] ),
+
+    'w_unexec' : ('UNEXEC_PROC_WEIGHT_SETUP',
+        'задает настройку весов неисполняемых процедур',
+        'disc', ['0', '1', '2'], None,
+        gl.__dict__['UNEXEC_PROC_WEIGHT_SETUP'] ),
+
+    'w_default' : ('DEFAULT_WEIGHT_FOR_PROC',
+        'задает вес по-умолчанию, который присваивается компилируемой, но неисполняемой процедуре',
+        'float', None, None,
+        gl.__dict__['DEFAULT_WEIGHT_FOR_PROC'] ),
+
+    'w_regn' : ('REGN_WEIGHT_SETUP',
+        'задает режим учета весов регионов фазы regions при обработке статистики работы компилятора LCC',
+        'disc', ['0', '1', '2'], None,
+        gl.__dict__['REGN_WEIGHT_SETUP'] ),
+
+    'w_node' : ('NODE_WEIGHT_SETUP',
+        'задает режим определения начального веса узлов регионов фазы regions при обработке статистики работы компилятора LCC',
+        'disc', ['0', '1', '2', '3'], None,
+        gl.__dict__['NODE_WEIGHT_SETUP'] ),
+
+    'w_icv_regn' : ('ICV_REGN_WEIGHT_SETUP',
+        'задает режим учета весов регионов фазы if\_conv при обработке статистики работы компилятора LCC',
+        'disc', ['0', '1', '2'], None,
+        gl.__dict__['ICV_REGN_WEIGHT_SETUP'] ),
+
+    'w_sect' : ('SECT_WEIGHT_SETUP',
+        'задает режим учета весов сливаемых участков фазы if\_conv при обработке статистики работы компилятора LCC',
+        'disc', ['0', '1'], None,
+        gl.__dict__['SECT_WEIGHT_SETUP'] ),
+
+    'use_unexec' : ('USE_UNEXEC_PROCS_IN_STAT',
+        'включает обработку статистики компиляции неисполняемых процедур',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['USE_UNEXEC_PROCS_IN_STAT']) ),
+
+    'din_regn' : ('DINUMIC_REGN_OPERS_NUM',
+        'включает учет динамического изменения числа операций в регионах во время компиляции на фазе regions',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['DINUMIC_REGN_OPERS_NUM']) ),
+
+    'din_proc' : ('DINUMIC_PROC_OPERS_NUM',
+        'включает учет динамического изменения числа операций в процедуре во время компиляции на фазе regions',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['DINUMIC_PROC_OPERS_NUM']) ),
+
+    'file_w_task' : ('TASK_WEIGHT_PATH',
+        'задает путь к файлу, из которого считываются веса задач',
+        'path_to_file', None, None,
+        gl.__dict__['TASK_WEIGHT_PATH'] ),
+
+    'stat' : ('STAT_PATH',
+        'задает путь каталогу со статистикой компиляции, собранной при значениях параметров оптимизирующих преобразований по-умолчанию',
+        'path_to_dir', None, None,
+        gl.__dict__['STAT_PATH'] ),
+
+    'din_stat' : ('DINUMIC_STAT_PATH',
+        'задает путь каталогу, в котором ИС собирает статистику компиляции',
+        'path_to_dir', None, None,
+        gl.__dict__['DINUMIC_STAT_PATH'] ),
+
+    'dir_w_proc' : ('PROC_WEIGHT_PATH',
+        'задает путь до каталога с файлами, определяющими веса процедур',
+        'path_to_dir', None, None,
+        gl.__dict__['PROC_WEIGHT_PATH'] ),
+
+    'database' : ('PAR_DISTRIBUTION_DATABASE',
+        'включает хранение в оперативной памяти всех распределений, получаемых по статистике работы компилятора LCC',
+        'bool', ['0', '1'], None,
+        int(gl.__dict__['PAR_DISTRIBUTION_DATABASE']) ),
+
+    'output' : ('OUTPUTDIR',
+        'задает каталог, в котором ИС формирует свои выходные файлы',
+        'path_to_dir', None, None,
+        gl.__dict__['OUTPUTDIR'] ),
+
+    'rewrite' : ('ALLOW_REWRITE_OUTPUT_FILES',
+        'включает режим перезаписи выходных файлов',
+        'bool',
+        ['0', '1'], None,
+        int(gl.__dict__['ALLOW_REWRITE_OUTPUT_FILES']) ),
+
+    }
+
+print (data)
--- choice/print_tex.py
+++ choice/print_tex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import global_vars as gl
@@ -51,39 +51,132 @@ def get_tex_pattern(varname):
 """
     return res
 
-"""
-Изменились глобальные переменные
-TASK_WEIGHT_SETUP
-
-Новые глобальные переменные
-UNEXEC_PROC_WEIGHT_SETUP
-DEFAULT_WEIGHT_FOR_PROC
-
-PROC_WEIGHT_PATH
-TASK_WEIGHT_PATH
-USE_UNEXEC_PROCS_IN_STAT
-STAT_PATH
-DINUMIC_STAT_PATH
-
-ALLOW_REWRITE_OUTPUT_FILES
-"""
-
 gnames = """
-UNEXEC_PROC_WEIGHT_SETUP
-DEFAULT_WEIGHT_FOR_PROC
-PROC_WEIGHT_PATH
-TASK_WEIGHT_PATH
-USE_UNEXEC_PROCS_IN_STAT
-STAT_PATH
-DINUMIC_STAT_PATH
-ALLOW_REWRITE_OUTPUT_FILES
-""".split('\n')[1:-1]
+\input{SPECS}
+\input{SYNCHRONOUS_OPTIMIZATION_FOR_SPECS}
+\input{SEQ_OPTIMIZATION_WITH_STRATEGY}
+\input{OPTIMIZATION_STRATEGY}
+\input{GAIN_STAT_ON_EVERY_OPTIMIZATION_STEP}
+\input{START_TEMPERATURE}
+\input{TEMPERATURE_LAW_TYPE}
+\input{ALPHA_IN_TEPMERATURE_LAW}
+\input{DISTRIBUTION_LAW_TYPE}
+\input{USE_RELATIONS_OF_PARAMETORS}
+\input{TIME_EXEC_IMPOTANCE}
+\input{TIME_COMP_IMPOTANCE}
+\input{MEMORY_IMPOTANCE}
+\input{COMP_TIME_INCREASE_ALLOWABLE_PERCENT}
+\input{EXEC_TIME_INCREASE_ALLOWABLE_PERCENT}
+\input{MEMORY_INCREASE_ALLOWABLE_PERCENT}
+\input{MAX_NUMBER_ITERATIONS}
+\input{MAX_NUMBER_OF_ATTEMPTS_FOR_ITERATION}
+\input{DECREASE_TEMPERATURE_BEFORE_UNFORTUNATE_ITERATIONS}
+\input{MAX_DCS_LEVEL}
+\input{DCS_KOEF_LOOP_IMPOTANCE}
+\input{DCS_KOEF_EDGE_IMPOTANCE}
+\input{DCS_KOEF_NODE_IMPOTANCE}
+\input{DSC_IMPOTANCE_LIMIT}
+\input{SMOOTH_STAT}
+\input{ERF_KOEF_FOR_DISCRETE_PAR}
+\input{ERF_KOEF_FOR_CONTINUOUS_PAR}
+\input{ZERO_LIMIT_FOR_ERF}
+\input{ZERO_LIMIT_FOR_WEIGHT}
+\input{TASK_WEIGHT_SETUP}
+\input{PROC_WEIGHT_SETUP}
+\input{UNEXEC_PROC_WEIGHT_SETUP}
+\input{DEFAULT_WEIGHT_FOR_PROC}
+\input{REGN_WEIGHT_SETUP}
+\input{NODE_WEIGHT_SETUP}
+\input{ICV_REGN_WEIGHT_SETUP}
+\input{SECT_WEIGHT_SETUP}
+\input{USE_UNEXEC_PROCS_IN_STAT}
+\input{DINUMIC_REGN_OPERS_NUM}
+\input{DINUMIC_PROC_OPERS_NUM}
+""".split('\\input{')[1:]
+gnames = map(lambda x: x[:-2], gnames)
 
-print gnames
+gnames_plus = ['TASK_WEIGHT_PATH', 'STAT_PATH', 'DINUMIC_STAT_PATH', 'PROC_WEIGHT_PATH', 'PAR_DISTRIBUTION_DATABASE', 'OUTPUTDIR', 'ALLOW_REWRITE_OUTPUT_FILES']
 
-# gnames = get_globals_names()
-outputdir = './doc/tex_patterns/new/'
-for varname in gnames:
-    ofile = open(outputdir + varname + '.tex', 'w')
-    #ofile = None
-    print >> ofile, get_tex_pattern(varname)
+def print_brief_pars_desc(gnames, output = None):
+    rdir = './doc/global describtion/'
+    #print("\\begin{itemize}",file=output)
+    for var in gnames:
+        rfile = open(rdir + var + '.tex', 'r')
+        pvar = var.replace('_', '\\_')
+        text = rfile.read() 
+        desc = text.split('\\subsection*{' + pvar + '}')[1].split('\\subsubsection*{Тип параметра}')[0].strip()
+        ptype = text.split('\\subsubsection*{Тип параметра}')[1].split('\\subsubsection*{По умолчанию}')[0].strip()
+        print('\\item', pvar, '\\\\', file = output)
+        if ptype[-1] == '.':
+            dot = ''
+        else:
+            dot = '.'
+        print('Тип:', ptype[0].lower() + ptype[1:] + dot, file = output)
+        print(desc, file = output)
+        rfile.close()
+    #print("\\end{itemize}",file=output)
+
+def print_python_dict(gnames, tab = '    '):
+    rdir = './doc/global describtion/'
+    #print("\\begin{itemize}",file=output)
+    i = 0
+    for var in gnames:
+        rfile = open(rdir + var + '.tex', 'r')
+        pvar = var.replace('_', '\\_')
+        text = rfile.read() 
+        desc = text.split('\\subsection*{' + pvar + '}')[1].split('\\subsubsection*{Тип параметра}')[0].strip().replace('\n', ' ')
+        ptype = text.split('\\subsubsection*{Тип параметра}')[1].split('\\subsubsection*{По умолчанию}')[0].strip()
+        
+        def without_dot(word):
+            if word[-1] == '.':
+                return word[:-1]
+            else:
+                return word
+        def default_val(var):
+            val = gl.__dict__[var]
+            if type(val) != bool:
+                return 'gl.__dict__[\'' + var + '\']'
+            else:
+                return 'int(gl.__dict__[\'' + var + '\'])'
+        i += 1
+        def values(var, ptype):
+            val = gl.__dict__[var]
+            tmp = without_dot(ptype).replace('$', '').split(':')
+            if type(val) == bool:
+                return "['0', '1']"
+            elif len(tmp) == 2 and i >= 6:
+                if type(val) == float:
+                    return str([str(tmp[-1]).strip()])
+                else:
+                    return str(list(map(lambda x: x.strip(),  tmp[-1].split(','))))
+            else:
+                return 'None'
+        
+        tab2 = tab * 2
+        print(tab + '\'' + str(i) + '\'', ': (\'' + var + '\'', end=',\n')
+        print(tab2 + '\'' + without_dot(desc).lower() + '\'', end=',\n')
+        print(tab2 + '\'' + ptype.lower() + '\'', end=', ')
+        print(values(var, ptype), end=', ') # возможные значения
+        print('None', end=',\n') # формат
+        print(tab2 + default_val(var), end = ' ') # значение по-умолчанию
+        print('),')
+        print()
+        rfile.close()
+
+
+def get_globals_names(types = None):
+    """ Получить имена всех глобальных переменных проекта, [тип данных которых находится среди types]
+    """
+    def filterfunc(objname):
+        if type(gl.__dict__[objname]) in types:
+            return True
+        else:
+            return False
+        
+    result = [x for x in gl.__dict__.keys() if x[0] != '_'] # игнорируем переменные, которые начинаются с символа '_'
+    if types != None:
+        return list(filter(filterfunc, result))
+    else:
+        return result
+
+print_python_dict(list(gnames) + gnames_plus)
